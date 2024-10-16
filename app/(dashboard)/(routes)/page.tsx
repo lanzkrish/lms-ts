6cc52193c3b9b25ef601d50707d540cd31e49346
Dhananjay Sahoo <kanha94377@gmail.com>
2024-10-16 21:02:38 +0530

    unreleased v 0.1.0

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Home</h1>
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
+}
