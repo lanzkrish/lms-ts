885536af4c7a7aef4de490fd6f2fcf66d73b0ec3
Dhananjay Sahoo <kanha94377@gmail.com>
2024-10-17 20:19:21 +0530

    v0.1.3_preRelease

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const Logo = () => {
+    return (    
+        <Image
+            height={130}
+            width={130}
+            alt="Interncify"
+            src="/logo-interncify.svg"
+            />
+     );
+}
+ 
