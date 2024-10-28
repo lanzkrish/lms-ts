a60e3f536a5f3500b4fe1d999b5e442ff13773bb
Dhananjay Sahoo <kanha94377@gmail.com>
2024-10-28 23:14:51 +0530

    v0.1.17_preRelease Issue still persist

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,7 +2,7 @@ import { getChapter } from "@/actions/get-chapter";
 import { Banner } from "@/components/banner";
 
 import { auth } from "@clerk/nextjs/server";
-import { Video } from "lucide-react";
+import { File, Video } from "lucide-react";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/video-player";
 import { CourseEnrollButton } from "./_components/course-enroll-button";
@@ -91,12 +91,29 @@ const ChapterIdPage = async({
                     </div>
                     <Separator/>
                     <div>
-                        
+                        {
+                            <div> Hello
+                                </div>
+                        }
                         {/* <Preview value={chapter.description!}/> */}
                     </div>
                     {!!attachments.length && (
                         <>
                             <Separator/>
+                            <div className="p-4">
+                                {attachments.map((attachment) => (
+                                    <a 
+                                    href={attachment.url}
+                                    target="_blank"
+                                    key={attachment.id}
+                                    className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                                    >
+                                        <File/>
+                                        <p className="line-clamp-1"
+                                        >{attachment.name}</p>
+                                    </a>
+                                ))} …
+                            </div>
                         </>
                     )}
                 </div>
